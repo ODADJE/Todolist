ea21b6ce61e9c4fcfa3aa664cce3f792be596cce
ODADJE <odadjerk@gmail.com>
2022-06-19 23:15:14 +0000

    update

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,24 @@
 let compteur = 1;
 let j = 0
+let test = 0
 let tab = [];
 let newTab = [];
 tab[0] = 1;
 const addTask = () => {
-  compteur++;
-  const newTask = document.querySelector(".myTask");
-
+    const newTask = document.querySelector(".myTask");
+    if(newTask.value == ""){
+        alert("Please write something")
+        return;
+    }
+    compteur++;
+    tab.push(compteur);
+    if (tab.length != 0 && test == 1) {
+        const message = document.getElementById('p');
+        message.remove()
+        document.querySelector(".deleteTask").disabled = false;
+        test = 0
+        }
+  
   const divTask = document.createElement("Div");
   divTask.id = `div${compteur}`;
   document.querySelector(".tasks").appendChild(divTask);
@@ -21,11 +33,11 @@ const addTask = () => {
   taskCheck.type = `checkbox`;
   taskCheck.setAttribute("onchange", "checkedTask()");
   document.querySelector(`#div${compteur}`).appendChild(taskCheck);
-  tab.push(compteur);
   newTask.value = "";
 };
 
 const deleteTask = () => {
+    
   for (let index = 0; index < tab.length; index++) {
     let check = document.getElementById(`${tab[index]}`);
     if (check.checked === true) {
@@ -45,6 +57,17 @@ const deleteTask = () => {
     
   }
    newTab.length =0;
+   if (tab.length == 0) {
+    const message = document.createElement("P");
+  message.innerText = "Empty";
+  message.id = `p`;
+  document.querySelector(`.tasks`).appendChild(message);
+  message.style.textAlign = "center";
+  message.style.fontSize = "20px";
+  message.style.color = "blue";
+  document.querySelector(".deleteTask").disabled = true;
+  test = 1
+   }
 };
 
 
